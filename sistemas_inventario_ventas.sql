5f727e2f94ddfbe224f291d59a35d8147e9b8bba
melvin hurtado <elreycrack38@gmail.com>
2026-05-14 21:50:46 -0600

    Agregado codigo insercion de usuarios y catalogo de productos

--- sistemas_inventario_ventas.sql
+++ sistemas_inventario_ventas.sql
@@ -109,3 +109,13 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+INSERT INTO usuarios (nombre_completo, usuario, password, rol) VALUES
+('Administrador Principal', 'admin','admin123','Administrador'),
+('Cajero de Turno','xajero1','ventas2024','Cajero');
+
+INSERT INTO productos (nombre_producto, categoria, stock, precio) VALUES
+('Laptop Dell Inspiron 15', 'Computadoras', 10, 650.00),
+('Mouse Inalámbrico Logitech', 'Accesorios', 25, 15.50),
+('Impresora Epson EcoTank', 'Oficina', 5, 210.00),
+('Resma de Papel Tamaño Carta', 'Papelería', 100, 4.25);
